b8c82ada1e1850dc7476af21261b5179913fcd57
Craxer <ibnu.treasury@gmail.com>
2023-04-13 09:58:15 +0700

    update sk2

--- deploy.py
+++ deploy.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,ExtraTreesRegressor
-from sklearn.model_selection import validation_curve, LeaveOneOut, train_test_split, cross_val_score
-from sklearn.model_selection import cross_validate, KFold, cross_val_score, RandomizedSearchCV, GridSearchCV
+# from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,ExtraTreesRegressor
+# from sklearn.model_selection import validation_curve, LeaveOneOut, train_test_split, cross_val_score
+# from sklearn.model_selection import cross_validate, KFold, cross_val_score, RandomizedSearchCV, GridSearchCV
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
