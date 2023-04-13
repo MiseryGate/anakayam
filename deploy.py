85fa03c7f655b3614edf2e49d2c5c90eb717ea3f
Craxer <ibnu.treasury@gmail.com>
2023-04-13 10:14:49 +0700

    update req2

--- deploy.py
+++ deploy.py
@@ -3,6 +3,7 @@ import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,ExtraTreesRegressor
